e896ef8077088f97ff45eb1c4f9f821de747a423
Mahbub2001 <imahbubturza@gmail.com>
2025-03-04 11:47:58 +0600

    SearchBox

--- src/components/FileExplorar/FileExplorer.js
+++ src/components/FileExplorar/FileExplorer.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import SearchBox from "../SearchBox/SearchBox";
 
 export default function FileExplorer() {
   const [entries, setEntries] = useState([]);
@@ -41,49 +42,18 @@ export default function FileExplorer() {
 
   async function goBack() {
     if (handleStack.length > 1) {
-      handleStack.pop(); // Remove current folder
-      const parentHandle = handleStack[handleStack.length - 1]; // Get previous folder
+      handleStack.pop();
+      const parentHandle = handleStack[handleStack.length - 1];
       setCurrentHandle(parentHandle);
       await readDirectory(parentHandle);
-      setHandleStack([...handleStack]); // Update stack state
+      setHandleStack([...handleStack]);
     }
   }
 
   return (
     <div>
-      <div class="max-w-sm mx-auto border border-gray-600 rounded-sm shadow-lg">
-        <div className="flex items-center w-full px-5">
-          <label for="simple-search" class="sr-only">
-            Search
-          </label>
-          <div class="relative w-full">
-            <input
-              type="text"
-              id="simple-search"
-              class="text-sm rounded-lg block w-full p-2 focus:outline-0"
-              placeholder="Search branch name..."
-              required
-            />
-          </div>
-          <button type="submit" class="">
-            <svg
-              class="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-            <span class="sr-only">Search</span>
-          </button>
-        </div>
+      <div className="max-w-sm mx-auto border border-gray-600 rounded-sm shadow-lg">
+        <SearchBox />
       </div>
 
       <button onClick={handleFolderSelect}>Select Folder</button>
---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function SearchBox() {
+  return (
+    <div className="flex items-center w-full px-5">
+      <label htmlFor="simple-search" className="sr-only">
+        Search
+      </label>
+      <div className="relative w-full">
+        <input
+          type="text"
+          id="simple-search"
+          className="text-sm rounded-lg block w-full p-2 focus:outline-0"
+          placeholder="Search branch name..."
+          required
+        />
+      </div>
+      <button type="submit" className="cursor-pointer">
+        <svg
+          className="w-4 h-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+        <span className="sr-only">Search</span>
+      </button>
+    </div>
+  );
+}
+
+export default SearchBox;
